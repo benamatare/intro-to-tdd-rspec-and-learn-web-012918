0945f8e0d7cf665bdce1f5e9562da65dd770846d
Ben Matare <ben.a.matare@gmail.com>
2018-01-12 00:26:51 +0000

    automatically backed up by learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -3,7 +3,7 @@ require_relative './current_age_for_birth_year'
 puts "What year were you born?"
   birth_year = gets.to_i
 
-  users_age = 
+  users_age =
   current_age_for_birth_year(birth_year) +
   (Time.now.year - 2003)
 
